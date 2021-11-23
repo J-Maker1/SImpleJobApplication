263eb82e75bbc578159679043864816e5ec649e5
Justin McGough <72327349+J-Maker1@users.noreply.github.com>
2021-11-23 13:48:08 -0700

    Revert "login"
    This reverts commit 974360c01bb00bcdf196ccaeff3ea50ca3510d04.

--- Job App/Job App/Login.cs
+++ Job App/Job App/Login.cs
@@ -26,26 +26,5 @@ namespace Job_App
         {
             Application.Exit();
         }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-
     }
 }
