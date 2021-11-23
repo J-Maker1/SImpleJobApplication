974360c01bb00bcdf196ccaeff3ea50ca3510d04
hydrohd <eaescob3@asu.edu>
2021-11-23 13:40:46 -0700

    login

--- Job App/Job App/Login.cs
+++ Job App/Job App/Login.cs
@@ -26,5 +26,26 @@ namespace Job_App
         {
             Application.Exit();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
