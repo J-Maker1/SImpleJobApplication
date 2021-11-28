668ba86d290375be8cb2e49cc6f368770f6bb0f3
hydrohd <eaescob3@asu.edu>
2021-11-27 17:09:40 -0700

    login update, registering almost working

--- Job App/Job App/Login.Designer.cs
+++ Job App/Job App/Login.Designer.cs
@@ -37,6 +37,16 @@ namespace Job_App
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
@@ -83,9 +93,9 @@ namespace Job_App
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(92, 100);
+            this.button1.Location = new System.Drawing.Point(97, 100);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(52, 23);
+            this.button1.Size = new System.Drawing.Size(43, 23);
             this.button1.TabIndex = 5;
             this.button1.Text = "login";
             this.button1.UseVisualStyleBackColor = true;
@@ -97,25 +107,123 @@ namespace Job_App
             this.comboBox1.Items.AddRange(new object[] {
             "Applicant",
             "Employer"});
-            this.comboBox1.Location = new System.Drawing.Point(201, 57);
+            this.comboBox1.Location = new System.Drawing.Point(208, 61);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(74, 21);
             this.comboBox1.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(235, 41);
+            this.label4.Location = new System.Drawing.Point(217, 45);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(52, 13);
             this.label4.TabIndex = 7;
             this.label4.Text = "User type";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(97, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "register";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(11, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 22);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "New User";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 193);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "username";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 219);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "password";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(19, 167);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(70, 164);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(70, 190);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(70, 216);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(217, 177);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "User type";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Applicant",
+            "Employer"});
+            this.comboBox2.Location = new System.Drawing.Point(208, 193);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(74, 21);
+            this.comboBox2.TabIndex = 16;
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(330, 138);
+            this.ClientSize = new System.Drawing.Size(292, 275);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
@@ -143,6 +251,16 @@ namespace Job_App
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
--- Job App/Job App/Login.cs
+++ Job App/Job App/Login.cs
@@ -34,12 +34,12 @@ namespace Job_App
             var cs = "Host=ec2-44-198-211-34.compute-1.amazonaws.com;Username=pbjcyutaadgyaj;Password=d759389ade78e61ecc88e682dac3cbeacfe8bebd9c262af20d864dba3f4b1fbb;Database=d2iga4s88pcpv9";
             var con = new NpgsqlConnection(cs);
             con.Open();
-
             if (comboBox1.Text == "Applicant")
             {
-                string query = String.Format("SELECT ap.username, ap.password FROM Applicant AS ap WHERE ap.username = \'{0}\'", entrUsr);
+                string query = String.Format("SELECT ap.password FROM Applicant AS ap WHERE ap.username = \'{0}\';", entrUsr);
                 var cmd = new NpgsqlCommand(query, con);
                 NpgsqlDataReader rdr = cmd.ExecuteReader();
+
 
                 if (!rdr.Read())
                 {
@@ -47,14 +47,94 @@ namespace Job_App
                 }
                 else
                 {
-                    Console.WriteLine("{0}, {1}", rdr.GetString(0), rdr.GetString(1));
+
+                    string actpass = rdr.GetString(0);
+                    actpass = actpass.Trim(' ');
+                    //Console.WriteLine("{0}, {1}", rdr.GetString(0), rdr.GetString(1));
+                    //Comparing passwords (we should probably be storing the
+                    //hashes of the passwords but this is just for practice)
+                    Boolean passmatch = entrPass.Equals(actpass);
+
+                    if (passmatch)
+                    {
+                        Program.userPage.Show();
+                        this.Hide();
+                    }
+
                 }
 
             }
             else if (comboBox1.Text == "Employer")
             {
+                string query = String.Format("SELECT ap.password FROM Employer AS ap WHERE ap.username = \'{0}\';", entrUsr);
+                var cmd = new NpgsqlCommand(query, con);
+                NpgsqlDataReader rdr = cmd.ExecuteReader();
+
+
+                if (!rdr.Read())
+                {
+                    Console.WriteLine("No match");
+                }
+                else
+                {
+                    string actpass = rdr.GetString(0);
+                    actpass = actpass.Trim(' ');
+                    //Console.WriteLine("{0}, {1}", rdr.GetString(0), rdr.GetString(1));
+                    //Comparing passwords (we should probably be storing the
+                    //hashes of the passwords but this is just for practice)
+                    Boolean passmatch = entrPass.Equals(actpass);
+
+                    if (passmatch)
+                    {
+                        Program.employerPage.Show();
+                        this.Hide();
+                    }
+
+                }
+            }
+        }
+
+        //This is the method for the register button being clicked
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string entrUsr = textBox4.Text; string entrPass = textBox5.Text;
+            string entrName = textBox3.Text;  string usrType = comboBox2.Text;
+            var cs = "Host=ec2-44-198-211-34.compute-1.amazonaws.com;Username=pbjcyutaadgyaj;Password=d759389ade78e61ecc88e682dac3cbeacfe8bebd9c262af20d864dba3f4b1fbb;Database=d2iga4s88pcpv9";
+            var con = new NpgsqlConnection(cs);
+            con.Open();
+
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            if(usrType.Equals("Applicant"))
+            {
+                cmd.CommandText = String.Format(
+                    "INSERT INTO Applicant(user_id, username, password, applicant_name) VALUES(\'{0}\', \'{1}\', \'{2}\', \'{3}\')",
+                    Guid.NewGuid().ToString("N"), entrUsr, entrPass, entrName);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Applicant created");
 
             }
+            else if(usrType.Equals("Employer"))
+            {
+                cmd.CommandText = String.Format(
+                    "INSERT INTO Employer(user_id, username, password, company_name) VALUES(\'{0}\', \'{1}\', \'{2}\', \'{3}\')",
+                    Guid.NewGuid().ToString("N"), entrUsr, entrPass, entrName);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Employer created");
+                textBox4.Text
+
+            }
+
+
+            con.Close();
+
+        }
+
+        //Ignore
+        private void label10_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
